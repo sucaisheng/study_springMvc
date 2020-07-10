fa58b07880da4bfb472faf02465f96bee1c95afe
sucaisheng <1206468307@qq.com>
2020-07-10 23:49:32 +0800

    注意引用包

--- src/com/sucaisheng/controller/ItemController.java
+++ src/com/sucaisheng/controller/ItemController.java
@@ -2,20 +2,25 @@ package com.sucaisheng.controller;
 
 import com.sucaisheng.pojo.Item;
 import com.sucaisheng.service.IItemService;
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * @Controller :
@@ -65,16 +70,29 @@ public class ItemController {
     }
 
     @RequestMapping("/updateItem.do")
-    public String updateItem(Item item, Model model){
-        System.out.println("515587458" + item);
+    public String updateItem(MultipartFile pictureFile, Item item, Model model, Boolean status, HttpServletRequest request){
+        //获取需要存储位置的真实路径
+        String realPath = request.getServletContext().getRealPath("pic");
+        //通过UUID获取一个随机字符串作为文件新的名字，避免上传同一个文件出现重名
+        String newFileName = UUID.randomUUID().toString().replace("-", "");
+        //获取文件名的后缀
+        String extension = FilenameUtils.getExtension(pictureFile.getOriginalFilename());
+        //真正储存的名字
+        newFileName = newFileName + "." + extension;
+        try {
+            pictureFile.transferTo(new File(realPath + "/" + newFileName));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        item.setPic(newFileName);
         int crows = iItemService.updateItem(item);
         if (crows > 0){
-            List<Item> itemList = iItemService.getAllItems();
-            model.addAttribute("itemList",itemList);
-            return "itemList";
+            //List<Item> itemList = iItemService.getAllItems();
+            model.addAttribute("item",item);
+            return "editItem";
         }
         else{
-            return "editList";
+            return "editItem";
         }
     }
 
